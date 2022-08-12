044438c5b3d9073618c519589374404ed399d975
YGACHA <109829831+YGACHA@users.noreply.github.com>
2022-08-12 14:51:11 -0500

    Reto 5

---
+++ reto5/src/main/java/codigo/App.java
@@ -0,0 +1,18 @@
+package codigo;
+import codigo.Vista.*;
+/**
+ * Hello world!
+ *
+ */
+public class App
+{ 
+
+    public static void main( String[] args )
+{
+    //System.out.println( "Hello World!" );
+    //Consulta1VO.valores();
+    new InterfazP();
+}
+}
+
+
